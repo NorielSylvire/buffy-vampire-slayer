36cec32941e0233f6a5e3c8debc26c3b249f0eb4
Flaviu E. Hongu <39311030+Fl4v1u5@users.noreply.github.com>
2020-10-21 17:48:33 +0200

    Added Slayer attributes and Game constructor
    Added the Game constructor so the compiler stops whining about it and added the basic Slayer attributes.

--- src/org/ucm/tp1/Logic/Game.java
+++ src/org/ucm/tp1/Logic/Game.java
@@ -1,5 +1,7 @@
 package org.ucm.tp1.Logic;
 
 public class Game {
-
+	public Game(long seed, Level level) {
+		// TODO fill your code
+	}
 }
--- src/org/ucm/tp1/Logic/GameObjects/Slayer.java
+++ src/org/ucm/tp1/Logic/GameObjects/Slayer.java
@@ -1,5 +1,10 @@
 package org.ucm.tp1.Logic.GameObjects;
 
 public class Slayer {
-
+	private int cost = 50, health = 3, shootFrequency = 1, damage = 1;
+	/* TODO When new types of damage are added, such as area or diagonal,
+	 * create an enum with the different types. */
+	
+	public static final String usageMsg = "Usage: [a]dd <x> <y>";
+	
 }
